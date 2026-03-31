98572db5dd5a96b3a81c8acee72245c56608d45a
alhilalfathi <alhilal.fathi@gmail.com>
2026-03-31 16:30:23 +0700

    chor: import userRouter to main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import express from "express";
 import {constants} from "node:http2";
+import userRouter from "./routes/users.router.js";
 
 const app = express()
 
 app.use(express.json())
+
+app.use("/user", userRouter)
 
 app.get("/", function(req,res){
     req.status(constants.HTTP_STATUS_OK).json({
