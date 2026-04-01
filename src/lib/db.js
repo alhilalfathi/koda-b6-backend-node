cc75a54cd8d53be61db1199b080483be11fa6ab6
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-01 16:45:08 +0700

    feat: add pg library to connect database

---
+++ src/lib/db.js
@@ -0,0 +1,13 @@
+import pg from "pg";
+
+const {Pool} = pg
+
+const pool = new Pool({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    ssl: process.env.PGSSLMODE === "require" ? { rejectUnauthorized: false } : false
+})
+
+export default pool
