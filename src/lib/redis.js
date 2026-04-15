b034fbd4c3725f5d7991652a84c271802066a4c0
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-15 13:26:59 +0700

    chore: fix config redis

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -1,15 +1,10 @@
 import { createClient } from 'redis'
 
-// const redisHost = process.env.REDIS_HOST || '127.0.0.1'
-// const redisPort = process.env.REDIS_PORT || '6379'
-// const redisPassword = process.env.REDIS_PASSWORD || ''
-
-const redisHost =redis
-const redisPort =63797
-const redisPassword=root
+const redisHost = process.env.REDIS_HOST || '127.0.0.1'
+const redisPort = process.env.REDIS_PORT || '6379'
 
 const redisClient = createClient({
-    url: `redis://:${redisPassword}@${redisHost}:${redisPort}`
+    url: `redis://${redisHost}:${redisPort}`
 })
 
 redisClient.on('error', (err) => console.error('Redis Client Error:', err))
